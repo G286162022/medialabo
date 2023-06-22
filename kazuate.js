584001708a799f8e4ce6e3c60e834d026fc2f0ab
G286162022 <228616@st.takushoku-u.ac.jp>
2023-06-22 17:25:47 +0900

    new

--- kazuate.js
+++ kazuate.js
@@ -2,6 +2,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え(デバッグ用): ' + kotae);
 
 let kaisu = 0;
+let stop = 0;
 
 hantei();
 hantei();
@@ -11,7 +12,7 @@ hantei();
 function hantei() {
     kaisu = kaisu + 1;
     let yoso = 4;
-    let stop = 0;
+
     console.log(kaisu + "回目の予想: " + yoso);
 
     if(stop===1 || kaisu>3){
