a73552d5a807535c5385867e99a2acb73d407d81
G286162022 <228616@st.takushoku-u.ac.jp>
2023-06-22 16:22:10 +0900

    数当てゲーム

---
+++ kazuate.js
@@ -0,0 +1,14 @@
+let kotae = Math.floor(math.random()+10) + 1;
+console.log('答え(デバッグ用): ' + kotae);
+
+let kaisu = 0;
+
+hantei();
+hantei();
+hantei();
+hantei();
+
+function hantei() {
+    let yoso = 4;
+    
+}
