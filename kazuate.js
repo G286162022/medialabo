58559e2bd73b09f78316fb4760ffcf49d18a8357
G286162022 <228616@st.takushoku-u.ac.jp>
2023-06-29 21:48:24 +0900

    new

--- kazuate.js
+++ kazuate.js
@@ -5,24 +5,30 @@ let kaisu = 0;
 let stop = 0;
 
 hantei();
-hantei();
-hantei();
-hantei();
+
 
 function hantei() {
     kaisu = kaisu + 1;
     let yoso = 4;
-
+    let kaisu1 = document.querySelector('span#kaisu');
+    let yoso1 = document.querySelector('span#answer');
+    kaisu1.textContent = kaisu;
+    yoso1.textContent = yoso;
     console.log(kaisu + "回目の予想: " + yoso);
+    let result = document.querySelector('p#result');
 
     if(stop===1 || kaisu>3){
         console.log("答えは " + kotae + " でした。すでにゲームは終わっています");
+        result.textContent = '答えは ' + kotae + ' でした。すでにゲームは終わっています';
     }else if(kotae===yoso) {
         console.log("正解です。おめでとう！");
+        result.textContent = '正解です。おめでとう！';
         stop = stop + 1;
     }else if(kotae>yoso) {
         console.log("まちがい、答えはもっと大きいですよ");
+        result.textContent = 'まちがい、答えはもっと大きいですよ';
     }else if(kotae<yoso) {
         console.log("まちがい、答えはもっと大きいですよ");
+        result.textContent = 'まちがい、答えはもっと大きいですよ';
     }
 }
